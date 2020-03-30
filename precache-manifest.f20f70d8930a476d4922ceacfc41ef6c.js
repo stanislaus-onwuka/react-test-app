a735b8096a79c7c57db17f4187647dfc2ab30c2b
Onwuka Stanley <kachi2505@yahoo.com>
2020-03-30 13:39:13 +0000

    Updates

---
+++ precache-manifest.f20f70d8930a476d4922ceacfc41ef6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfef1b0e0fadde298bc25fcf1b91acee",
+    "url": "/react-test-app/index.html"
+  },
+  {
+    "revision": "de963f5225e40d595df7",
+    "url": "/react-test-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5ad568b0e1f0f58113a4",
+    "url": "/react-test-app/static/js/2.98e0e3d9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-test-app/static/js/2.98e0e3d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de963f5225e40d595df7",
+    "url": "/react-test-app/static/js/main.2965816a.chunk.js"
+  },
+  {
+    "revision": "4ed3a998ff01e0a2ba1b",
+    "url": "/react-test-app/static/js/runtime-main.67af1708.js"
+  }
+]);
